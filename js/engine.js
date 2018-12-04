6b3e8eb2caca6a0751102a9564c8ff1d772acd7c
bmcardoso <bcardoso.mail@gmail.com>
2018-12-04 20:37:29 +0000

    update code base

--- js/engine.js
+++ js/engine.js
@@ -1,21 +1,44 @@
 const SURVIVORS = [ "dwight",  "meg", "claudette", "jake", "nea", "bill", "david", "laurie", "ace", "feng", "quentin",
     "tapp", "kate", "francis" ];
 
+const KILLERS = [ "clown",  "doctor", "freddy", "hag", "hillbilly", "huntress", "leatherface", "myers", "nurse", "pig", "spirit",
+    "trapper", "wraith" ];
+
+const ROLES = [ "killer", "survivor" ];
+
 class Engine {
 
-    getRandomSurvivor() {
-        return SURVIVORS[this._getRandomNumberBetween(0, SURVIVORS.length - 1)];
+    pickRandomRole() {
+        return this._pickRandom(ROLES);
     }
 
-    getSurvivorRandomPerks(){
-        return this._getRandomNumbersBetween(4, 1, 58);
+    pickRandomCharacter(role) {
+        switch(role) {
+            case ROLES[0]:
+                return this._pickRandom(KILLERS)
+            case ROLES[1]:
+                return this._pickRandom(SURVIVORS)
+        }
+    }
+
+    pickRandomPerks(role){
+        switch(role) {
+            case ROLES[0]:
+                return this._getRandomNumbersBetween(4, 1, 49);
+            case ROLES[1]:
+                return this._getRandomNumbersBetween(4, 1, 58);
+        }
+    }
+
+    _pickRandom(array) {
+        return array[this._getRandomNumberBetween(0, array.length)];
     }
 
     _getRandomNumbersBetween(quantity, min, max) {
-        var result = [];
+        let result = [];
 
         while (result.length < quantity) {
-            var number = this._getRandomNumberBetween(1, 58);
+            let number = this._getRandomNumberBetween(min, max);
 
             if(!result.includes(number)) {
                 result.push(number);
@@ -30,20 +53,24 @@ class Engine {
     }
 }
 
-var engine = new Engine();
+let engine = new Engine();
 
 function randomize() {
-    document.getElementById('portrait').style.backgroundImage = "url('img/portraits/survivors/" + engine.getRandomSurvivor() + ".png')";
+    let role = engine.pickRandomRole();
 
-    engine.getSurvivorRandomPerks().forEach(function(perk, i) {
-        document.getElementsByClassName('perk')[i].style.backgroundImage = "url('img/perks/survivor/" + perk + ".png')";
+    document.getElementById("title").innerText = role;
+
+    document.getElementById('portrait').style.backgroundImage = "url('img/portraits/" + role + "/" + engine.pickRandomCharacter(role) + ".png')";
+
+    engine.pickRandomPerks(role).forEach(function(perk, i) {
+        document.getElementsByClassName('perk')[i].style.backgroundImage = "url('img/perks/" + role + "/" + perk + ".png')";
     });
 }
 
 window.onload = function() {
     randomize();
 
-    document.getElementById('randomize').addEventListener("click", function (ev) {
+    document.getElementById('randomize').addEventListener("click", function () {
         randomize();
     });
-}
+};
