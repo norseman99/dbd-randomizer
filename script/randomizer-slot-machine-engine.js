c4946951e07febb04cb7118ae6a275563ba58333
bmcardoso <bcardoso.mail@gmail.com>
2018-12-30 11:36:38 +0000

    add nurse addons

--- script/randomizer-slot-machine-engine.js
+++ script/randomizer-slot-machine-engine.js
@@ -263,7 +263,7 @@ class RandomizerSlotMachineEngine {
 
     randomizeItems() {
         let role = this.getActiveRole();
-        let character = 'myers';
+        let character = 'nurse';
 
         this.uiHandler.toggleAddonsBlankBackground(false);
         RandomizerUiGenerator.generateCharacterSpecificElements(role, character);
